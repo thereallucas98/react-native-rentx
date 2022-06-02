e6927657b730218e361517456bc3caef02aa9d9a
thereallucas98 <lucas1998david@gmail.com>
2022-06-02 07:59:27 -0300

    Componente Botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { RectButtonProps } from 'react-native-gesture-handler';
-import { ActivityIndicator } from 'react-native';
+import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
+import { ActivityIndicator } from "react-native";
 
-import {
-  Container,Title
-} from './styles';
-import theme from '../../styles/theme';
+import { Container, Title } from "./styles";
+import theme from "../../styles/theme";
 
-interface Props extends RectButtonProps{
-    title: string;
-    color?: string;
-    enabled?: boolean;
-    loading?: boolean;
+interface Props extends RectButtonProps {
+  title: string;
+  color?: string;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({title, enabled = true, loading=false, ...rest}: Props) {
-
+export function Button({
+  title,
+  enabled = true,
+  loading = false,
+  ...rest
+}: Props) {
   return (
-    <Container 
-      {...rest} 
-      style={{opacity: (enabled === false || loading === true) ? .5 : 1}} 
-      enabled={true} 
+    <Container
+      {...rest}
+      style={{ opacity: enabled === false || loading === true ? 0.5 : 1 }}
+      enabled={true}
     >
-      { loading ? 
+      {loading ? (
         <ActivityIndicator color={theme.colors.shape} />
-       : <Title>{title}</Title>
-      }
-
+      ) : (
+        <Title>{title}</Title>
+      )}
     </Container>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,14 +3,17 @@ import styled from 'styled-components/native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
 interface ButtonProps extends RectButtonProps {
-    color?: string;
+  color?: string;
 }
 
 export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
+
   padding: 19px;
+  
   align-items: center;
   justify-content: center;
+  
   background-color: ${({ color, theme}) => color ? color : theme.colors.main};
 `;
 
