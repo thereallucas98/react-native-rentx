9e4077ea9cb59e1b26df3c829efe49eb1467b974
thereallucas98 <lucas1998david@gmail.com>
2022-06-03 09:18:28 -0300

    Interface do Agendamento Concluido

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -1,49 +1,45 @@
-import React from 'react';
-import { useWindowDimensions, StatusBar } from 'react-native';
-import LogoSvg from '../../assets/logo_background_gray.svg';
-import DoneSvg from '../../assets/done.svg';
+import React from "react";
+import { useWindowDimensions, StatusBar } from "react-native";
+import LogoSvg from "../../assets/logo_background_gray.svg";
+import DoneSvg from "../../assets/done.svg";
 
-import { ConfirmButton } from '../../components/ConfirmButton';
-import {
-  Container,Content,Title,Message,Footer
-} from './styles';
-import { useNavigation } from '@react-navigation/native';
-
+import { ConfirmButton } from "../../components/ConfirmButton";
+import { Container, Content, Title, Message, Footer } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 export function SchedulingComplete() {
-    const { width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    function handleConfirm(){
-      navigation.navigate('Home');
-    };
-  
+  function handleConfirm() {
+    navigation.navigate("Home");
+  }
+
   return (
     <Container>
-      <StatusBar 
-        barStyle='light-content'
+      <StatusBar
+        barStyle="light-content"
         translucent
-        backgroundColor='transparent'
+        backgroundColor="transparent"
       />
 
-        <LogoSvg width={width} />
+      <LogoSvg width={width} />
 
-        <Content>
-            <DoneSvg width={80} height={80} />
-            <Title>Carro alugado</Title>
+      <Content>
+        <DoneSvg width={80} height={80} />
+        <Title>Carro alugado</Title>
 
-            <Message>
-                Agora você só precisa ir {'\n'}
-                até a concessionária da RENTX {'\n'}
-                pegar o seu automóvel.
-            </Message>
-        </Content>
+        <Message>
+          Agora você só precisa ir {"\n"}
+          até a concessionária da RENTX {"\n"}
+          pegar o seu automóvel.
+        </Message>
+      </Content>
 
-        <Footer>
-            <ConfirmButton title='OK' onPress={handleConfirm} />
-        </Footer>
-
+      <Footer>
+        <ConfirmButton title="OK" onPress={handleConfirm} />
+      </Footer>
     </Container>
   );
 }
--- src/screens/SchedulingComplete/styles.ts
+++ src/screens/SchedulingComplete/styles.ts
@@ -9,31 +9,35 @@ export const Container = styled.View`
 `;
 
 export const Content = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
+flex: 1;
+justify-content: center;
+align-items: center;
 
-    padding-bottom: 40px;
+padding-bottom: 40px;
 `;
 
 export const Title = styled.Text`
-    font-size: ${RFValue(30)}px;
-    color: ${({ theme }) => theme.colors.shape};
-    font-family: ${({ theme }) => theme.fonts.secondary_600};
-    margin-top: 40px;
+font-size: ${RFValue(30)}px;
+color: ${({ theme }) => theme.colors.shape};
+font-family: ${({ theme }) => theme.fonts.secondary_600};
+
+margin-top: 40px;
 `;
 
 export const Message = styled.Text`
-    font-size: ${RFValue(15)}px;
-    color: ${({ theme }) => theme.colors.text_detail};
-    font-family: ${({ theme }) => theme.fonts.primary_400};
-    line-height: ${RFValue(25)}px;
-    text-align: center;
-    margin-top: 16px;
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  line-height: ${RFValue(25)}px;
+  text-align: center;
+  
+  margin-top: 16px;
 `;
 
 export const Footer = styled.View`
-    width: 100%;
-    align-items: center;
-    margin: 80px 0;
+  width: 100%;
+  
+  align-items: center;
+
+  margin: 80px 0;
 `;
