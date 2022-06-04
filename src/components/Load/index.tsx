d92feafc840894891f7df61c3e39d3a06e4f2ad2
thereallucas98 <lucas1998david@gmail.com>
2022-06-04 16:38:38 -0300

    Listando carros da API

--- src/components/Load/index.tsx
+++ src/components/Load/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
-import { useTheme } from 'styled-components';
-
+import React from "react";
+import { ActivityIndicator } from "react-native";
+import { useTheme } from "styled-components";
 
 export function Load() {
   const theme = useTheme();
   return (
-    <ActivityIndicator 
-        color={theme.colors.main}
-        size='large'
-        style={{ flex: 1 }}
+    <ActivityIndicator
+      color={theme.colors.main}
+      size="large"
+      style={{ flex: 1 }}
     />
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.10:3333',
+  baseURL: "http://192.168.27.106:3333",
 });
 
 export { api };
