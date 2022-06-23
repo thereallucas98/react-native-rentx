72fd715c8d491d719a0ea5ab7b15d7c74b31f9b1
thereallucas98 <lucas1998david@gmail.com>
2022-06-23 18:50:49 -0300

    Refactor do Botão

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,8 +1,8 @@
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
-import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { RectButton } from "react-native-gesture-handler";
 
-interface ButtonProps extends RectButtonProps {
+interface ButtonProps {
   color?: string;
 }
 
