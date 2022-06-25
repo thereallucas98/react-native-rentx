ab6bcbc87222c6a5900627aea85c14143445fa6f
thereallucas98 <lucas1998david@gmail.com>
2022-06-25 13:57:09 -0300

    Atualizando tela de confirmação

--- src/screens/Confirmation/styles.ts
+++ src/screens/Confirmation/styles.ts
@@ -1,9 +1,5 @@
-import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
@@ -15,15 +11,13 @@ export const Content = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-
-  padding-bottom: 40px;
+  padding-bottom: 80px;
 `;
 
 export const Title = styled.Text`
   font-size: ${RFValue(30)}px;
   color: ${({ theme }) => theme.colors.shape};
   font-family: ${({ theme }) => theme.fonts.secondary_600};
-
   margin-top: 40px;
 `;
 
@@ -33,14 +27,11 @@ export const Message = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_400};
   line-height: ${RFValue(25)}px;
   text-align: center;
-
   margin-top: 16px;
 `;
 
 export const Footer = styled.View`
   width: 100%;
-
   align-items: center;
-
   margin: 80px 0;
 `;
