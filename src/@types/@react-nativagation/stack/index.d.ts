64743bdb2e8dbf710590d2968dd3bbd7cbc05e2a
thereallucas98 <lucas1998david@gmail.com>
2022-06-25 13:56:14 -0300

    Rotas

--- src/@types/@react-nativagation/stack/index.d.ts
+++ src/@types/@react-nativagation/stack/index.d.ts
@@ -1,11 +1,27 @@
+type ScreenDTO = {
+  nextScreenRoute: 'SignIn' | 'Home'
+  title: string
+  message: string
+}
+
 declare namespace ReactNavigation {
-  export interface RootParamList extends RootStackParamList {
-    Home: NavigationStackProp<string>;
-    CarDetails: NavigationStackProp<string>;
-    Scheduling: NavigationStackProp<string>;
-    SchedulingDetails: NavigationStackProp<string>;
-    SchedulingComplete: NavigationStackProp<string>;
-    MyCars: NavigationStackProp<string>;
-    SignIn: NavigationStackProp<string>;
+  export interface RootParamList {
+    Splash: undefined
+    SignUpFirstStep: undefined
+    SignUpSecondStep: {
+      user:
+        | {
+            name: string
+            email: string
+            driverLicense: string
+          }
+        | undefined
+    }
+    Home: { car: CarDTO | undefined }
+    CarDetails: { car: CarDTO | undefined }
+    Scheduling: { car: CarDTO | undefined }
+    SchedulingDetails: { car: CarDTO; dates: string[] }
+    Confirmation: ScreenDTO
+    MyCars: undefined
   }
 }
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,16 +6,18 @@ import { Home } from "../screens/Home";
 import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
-import { SchedulingComplete } from "../screens/SchedulingComplete";
+import { Confirmation } from "../screens/Confirmation";
 import { MyCars } from "../screens/MyCars";
 import { Splash } from "../screens/Splash";
 import { SignIn } from "../screens/SignIn";
+import { SignUpFirstStep } from "../screens/SignUp/FirstStep";
+import { SignUpSecondStep } from "../screens/SignUp/SecondStep";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }} initialRouteName="SignIn">
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="SignUpFirstStep">
       <Screen name="Splash" component={Splash} />
 
       <Screen
@@ -32,11 +34,15 @@ export function StackRoutes() {
 
       <Screen name="SchedulingDetails" component={SchedulingDetails} />
 
-      <Screen name="SchedulingComplete" component={SchedulingComplete} />
+      <Screen name="Confirmation" component={Confirmation} />
 
       <Screen name="MyCars" component={MyCars} />
 
       <Screen name="SignIn" component={SignIn} />
+
+      <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
+
+      <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
     </Navigator>
   );
 }
